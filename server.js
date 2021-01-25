7679b4c6d341a49779ac166f52616dbeeac14e39
BC7 <brian.ngobidi@gmail.com>
2021-01-25 12:40:51 -0500

    case sensitive env variable

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const routes = require('./routes');
-const PORT = process.env.port || 8000;
+const PORT = process.env.PORT || 8000;
 const app = express();
 
 app.use(express.static('public'));
